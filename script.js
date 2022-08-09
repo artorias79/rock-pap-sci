57d9814162fa3b0bdda10fb4fda5300b8f48bd70
Ahmed Awadelkareem <ahmdhelps@gmail.com>
2022-08-09 19:30:06 +0100

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function getComputerChoice() {
 	let numGen = Math.floor(Math.random() * 3);
 	console.log(numGen);
-	if
+	if (numGen == 0) {
+		return 'rock';
+	} else if (numGen == 1) {
+		return 'paper';
+	} else return 'scissors'
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
