4305f1011da575f8dd9f5982c93a29153f213da9
Ahmed Awadelkareem <ahmdhelps@gmail.com>
2022-08-09 19:48:13 +0100

    add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// function that generates a random number and returns either rock, paper or scissors depending on the number
+
 function getComputerChoice() {
 	let numGen = Math.floor(Math.random() * 3);
 	console.log(numGen);
@@ -8,4 +10,44 @@ function getComputerChoice() {
 	} else return 'scissors'
 }
 
-console.log(getComputerChoice());
+// assigned getComputerChoice to a variable for ease of use
+let playerChoice = 'rock';
+let computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+
+// function that plays one round
+
+function playRound(playerSelection, computerSelection) {
+	// if statement will exit the program if rock, paper or scissors is not played by the player
+	if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+		return 'That\'s not how you play the game!';
+	// going through all options if the player selection is rock	
+	} else if (playerSelection == 'rock') {
+		if (computerSelection == 'rock') {
+			return 'It\'s a draw';
+		} else if (computerSelection == 'paper') {
+			return 'You lose';
+		} else if (computerSelection == 'scissors') {
+			return 'You win';
+		}
+	// going though all options if the player selection is paper
+	} else if (playerSelection == 'paper') {
+		if (computerSelection == 'rock') {
+			return 'You win';
+		} else if (computerSelection == 'paper') {
+			return 'It\'s a draw';
+		} else if (computerSelection == 'scissors') {
+			return 'You lose';
+		}
+	// going though all options if the player selection is scissors
+	} else if (playerSelection == 'scissors') {
+		if (computerSelection == 'rock') {
+			return 'You lose';
+		} else if (computerSelection == 'paper') {
+			return 'You win';
+		} else if (computerSelection == 'scissors') {
+			return 'It\'s a draw';
+		}
+	}
+}
